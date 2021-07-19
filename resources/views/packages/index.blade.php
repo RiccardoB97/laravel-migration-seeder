a9e58b7329b8ef3e2f1612119ddd333e40b2a235
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-07-19 14:44:50 +0200

    Add cards in packages.index with each package shown

--- resources/views/packages/index.blade.php
+++ resources/views/packages/index.blade.php
@@ -1,5 +1,19 @@
 @extends('layout.app')
 @section('title', 'Home - Travel Packages')
 @section('main_content')
-
+<div class="container">
+    @foreach($packages as $package)
+    <div class="card">
+        <dl>
+            <dt>Package name:</dt>
+            <dd>{{$package->name}}</dd>
+            <dt>Description:</dt>
+            <dd>{{$package->description}}</dd>
+            <dt>Price:</dt>
+            <dd>{{$package->price}}</dd>
+            <img src="{{$package->image_url}}" alt="{{$package->name}}">
+        </dl>
+    </div>
+    @endforeach
+</div>
 @endsection
