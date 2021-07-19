5ac2a3413404ebadfcd400af695b5daf9ed23b4c
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-07-19 14:38:09 +0200

    Used faker in packageseeder and completed seeding in DB

--- database/seeds/PackageSeeder.php
+++ database/seeds/PackageSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Package;
 
 class PackageSeeder extends Seeder
 {
@@ -9,8 +11,16 @@ class PackageSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 15; $i++) { 
+            $package = new Package;
+            $package->name = $faker->sentence(3);
+            $package->description = $faker->paragraph();
+            $package->destination = $faker->country();
+            $package->price = $faker->numberBetween(1000, 3500);
+            $package->image_url = $faker->imageUrl(640, 480, 'packages', true,'travel_packages', true);
+            $package->save();
+        }
     }
 }
